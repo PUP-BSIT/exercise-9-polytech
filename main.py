431d2d04ad63458befc4c587af68ee752cd85244
raquem-annierose <annierosesraquem@iskolarngbayan.pup.edu.ph>
2025-04-08 21:52:47 +0800

    add group TODOs for Exercise 9

---
+++ main.py
@@ -0,0 +1,9 @@
+# Group: PolyTech | Data: Roblox Game Directory
+
+# TODO: Design Roblox game list and display functions. Assigned to: Zyra  
+# TODO: Add Roblox game details from user input. Assigned to: Annie  
+# TODO: Update a game using its Game ID. Assigned to: Keith  
+# TODO: Delete a game by Game ID. Assigned to: Mikee  
+# TODO: Search for a game by title. Assigned to: Kalelle  
+# TODO: Create and handle menu systems. Assigned to: Kalelle & Mikee  
+# TODO: Run program loop to execute all features. Assigned to: Everyone
