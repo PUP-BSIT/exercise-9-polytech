14281d2f5962c0a6345fbd7bf0037452be0f7a19
capilitan_mikee <mikee.capilitan@gmail.com>
2025-04-08 22:25:34 +0800

    add menu

--- main.py
+++ main.py
@@ -5,5 +5,16 @@
 # TODO: Update a game using its Game ID. Assigned to: Keith  
 # TODO: Delete a game by Game ID. Assigned to: Mikee  
 # TODO: Search for a game by title. Assigned to: Kalelle  
-# TODO: Create and handle menu systems. Assigned to: Kalelle & Mikee  
+
+def main_menu():
+# TODO: Create and handle menu systems. Assigned to: Kalelle & Mikee
+    while True:
+        print("\n-----[Roblox Game Menu]-----")
+        print("1. List Roblox Games")
+        print("2. Add Roblox Games")
+        print("3. Update Roblox Games")
+        print("4. Delete Roblox Games")
+        print("5. Search Roblox Games")
+        print("6. Exit")
+  
 # TODO: Run program loop to execute all features. Assigned to: Everyone
